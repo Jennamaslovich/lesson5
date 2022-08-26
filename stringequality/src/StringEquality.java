cc10fee132ef61f49c86b7bbe50f3df017f7e8c1
Jennamaslovich <108747993+Jennamaslovich@users.noreply.github.com>
2022-08-25 20:27:33 -0400

    string equality

---
+++ stringequality/src/StringEquality.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+
+
+public class StringEquality {
+    public static void main(String[] args) {
+        String rocknroll = "moe";
+        boolean music = rocknroll.equals("Moe");
+
+        if (music = false) { // false makes it "You are not the king" and true is "You are the king of rock and roll"
+            System.out.println("You are the king of rock and roll ");
+        } else {
+            System.out.println("You are not the king");
+        }
+
+    }
+}
+
